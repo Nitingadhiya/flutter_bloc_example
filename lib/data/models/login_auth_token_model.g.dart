3ec7c264b3b23f02fdd73ffe27bbe09400e26a06
Nitingadhiya <vthink.nitin@gmail.com>
2023-07-25 13:55:48 +0530

    Forgot to commit "model.g" file

---
+++ lib/data/models/login_auth_token_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_auth_token_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LoginAuthTokenModel _$LoginAuthTokenModelFromJson(Map<String, dynamic> json) =>
+    LoginAuthTokenModel(
+      authToken: json['token'] as String,
+    );
+
+Map<String, dynamic> _$LoginAuthTokenModelToJson(
+        LoginAuthTokenModel instance) =>
+    <String, dynamic>{
+      'token': instance.authToken,
+    };
